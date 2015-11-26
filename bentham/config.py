4eb619818010c9608d24d3e6c56c1559c8125fca
Christopher Kotfila <chris.kotfila@kitware.com>
2015-11-26 11:25:01 -0500

    Add use pkg_resource to access config file
    Also creates a conf/ directory,  and a conf/bentham-example.json which will
    have to be copied to the ignored conf/bentham.json file

--- bentham/config.py
+++ bentham/config.py
@@ -1,8 +1,10 @@
 import json
+import pkg_resources as pr
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
 def get_pg_db():
-    with open('../bentham.json', 'rb') as infile:
+    conf_file = pr.resource_filename('bentham', 'conf/bentham.json')
+    with open(conf_file, 'rb') as infile:
         credentials = json.load(infile)['database']
 
     db = PostgresqlExtDatabase(credentials['name'],
