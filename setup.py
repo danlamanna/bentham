e3255327edb5a5d8c175de5384ae41e620649987
Christopher Kotfila <chris.kotfila@kitware.com>
2015-11-26 11:04:15 -0500

    Add a setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bentham",
+    author="Dan Lamanna, Chris Kotfila",
+    author_email="danlamanna@gmail.com, kotfic@gmail.com",
+    license="",
+    description="",
+    version="0.1.0",
+    packages=['bentham']
+)
