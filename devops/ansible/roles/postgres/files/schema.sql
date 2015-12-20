92873811ad18f8c43e665f87f62514a16ef6daa0
Dan LaManna <me@danlamanna.com>
2015-12-19 21:49:38 -0500

    Add database schema

---
+++ devops/ansible/roles/postgres/files/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS events (
+       id SERIAL,
+       created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+       occurred_at TIMESTAMP WITH TIME ZONE NOT NULL,
+       tracker VARCHAR NOT NULL,
+       source_identifier VARCHAR NOT NULL,
+       message VARCHAR NOT NULL,
+       raw_event TEXT,
+       raw_event_json JSON,
+       ack BOOLEAN DEFAULT FALSE
+);
