69b7fb4c664a592e9373c06313acb2a95aac7689
Dan LaManna <me@danlamanna.com>
2015-12-21 22:22:26 -0500

    Add basic configuration management

--- bentham/config.py
+++ bentham/config.py
@@ -1,13 +1,24 @@
-import json
-import pkg_resources as pr
+import os
+import yaml
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
+def config_file():
+    if os.environ.has_key('BENTHAM_CONFIG'):
+        return os.environ.get('BENTHAM_CONFIG')
+    elif os.path.exists(os.path.expanduser('~/.config/bentham/config.yml')):
+        return os.path.expanduser('~/.config/bentham/config.yml')
+    elif os.path.exists('/etc/bentham.yml'):
+        return '/etc/bentham.yml'
+    else:
+        raise Exception('No configuration file found.')
+
+def get_config():
+    with open(config_file(), 'rb') as infile:
+        return yaml.load(infile)
+
 def get_pg_db():
-    conf_file = pr.resource_filename('bentham', 'conf/bentham.json')
-    with open(conf_file, 'rb') as infile:
-        credentials = json.load(infile)['database']
+    config = get_config()
 
-    db = PostgresqlExtDatabase(credentials['name'],
-                               **{k: v for k, v in credentials.items() if k != 'name'})
-
-    return db
+    return PostgresqlExtDatabase(config['datastore']['database'],
+                                 **{k: v for k, v in config['datastore'].items() if \
+                                    k not in ('type', 'database')})
